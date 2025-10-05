de709e0923237a8e0f0e894279298f178986fc02
Danulo <danulo1337@gmail.com>
2025-10-05 23:59:26 +0300

    create models according to DB structure & set AUTH_USER_MODEL in settings.py

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.username} {self.first_name} {self.last_name}"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Redactor)
+
+    def __str__(self):
+        return f"{self.title} (Topic: {self.content})"
