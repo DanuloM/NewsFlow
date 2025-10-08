3e909085083d875e072bc5ace50d5200fc1d747e
Danulo <danulo1337@gmail.com>
2025-10-08 23:23:19 +0300

    add content to index page

--- agency/views.py
+++ agency/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from agency.models import Newspaper, Topic, Redactor
+
 
 def index(request):
-    return render(request, "agency/index.html")
+    num_of_newspapers = Newspaper.objects.count()
+    num_of_topics = Topic.objects.count()
+    num_of_redactors = Redactor.objects.count()
+    context = {
+        "num_of_newspapers": num_of_newspapers,
+        "num_of_topics": num_of_topics,
+        "num_of_redactors": num_of_redactors,
+    }
+    return render(request, "agency/index.html", context)
